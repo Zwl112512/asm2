3b417c9b064bccc86cfd44a077a047e91b8cbe60
Zwl112512 <147674826+Zwl112512@users.noreply.github.com>
2025-01-26 02:42:36 +0800

    Update splash_screen.dart

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:provider/provider.dart';
 import '../providers/auth_provider.dart';
 
@@ -9,20 +10,41 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  String? imageUrl; // 存储 Firebase Storage 图片 URL
+  bool isImageLoaded = false;
+
   @override
   void initState() {
     super.initState();
+    _loadResources();
+  }
 
-    Future.delayed(Duration(seconds: 2), () {
-      final authProvider = Provider.of<CustomAuthProvider >(context, listen: false);
-      authProvider.checkLoginStatus(); // 同步登录状态
+  Future<void> _loadResources() async {
+    try {
+      // 获取图片 URL
+      final ref = FirebaseStorage.instance.ref('profile_images/mqMk7jUCbPStMv1iTA0VBXLZCxc2.jpg');
+      final url = await ref.getDownloadURL();
 
-      if (authProvider.user == null) {
-        Navigator.pushReplacementNamed(context, '/login'); // 未登录，跳转到登录页
-      } else {
-        Navigator.pushReplacementNamed(context, '/home'); // 已登录，跳转到主界面
-      }
-    });
+      // 加载完成后更新状态
+      setState(() {
+        imageUrl = url;
+        isImageLoaded = true;
+      });
+
+      // 延迟一定时间后导航
+      Future.delayed(Duration(seconds: 4), () {
+        final authProvider = Provider.of<CustomAuthProvider>(context, listen: false);
+        authProvider.checkLoginStatus();
+
+        if (authProvider.user == null) {
+          Navigator.pushReplacementNamed(context, '/login');
+        } else {
+          Navigator.pushReplacementNamed(context, '/home');
+        }
+      });
+    } catch (e) {
+      print('Error loading image: $e');
+    }
   }
 
   @override
@@ -33,7 +55,10 @@ class _SplashScreenState extends State<SplashScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset('assets/logo.jpg', height: 100),
+            if (isImageLoaded)
+              Image.network(imageUrl!, height: 100) // Firebase Storage 的图片
+            else
+              CircularProgressIndicator(), // 显示加载指示器
             SizedBox(height: 20),
             Text(
               'Food Delivery App',
