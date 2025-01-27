b770ec99be00e0b49bf4db19f5caabcd3608161b
Zwl112512 <147674826+Zwl112512@users.noreply.github.com>
2025-01-28 02:30:31 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:asm2/screens/RestaurantsOverviewPage.dart';
 import 'package:asm2/screens/profile_page.dart';
 import 'package:flutter/material.dart';
@@ -6,7 +5,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'providers/auth_provider.dart';
 import 'providers/cart_provider.dart';
-import 'screens/splash_screen.dart';
+
 import 'screens/home_screen.dart';
 import 'screens/login_page.dart';
 import 'screens/restaurants_page.dart';
@@ -21,7 +20,15 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
-  runApp(MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => CustomAuthProvider()),
+        ChangeNotifierProvider(create: (_) => CartProvider()),
+      ],
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -38,7 +45,6 @@ class MyApp extends StatelessWidget {
         '/home': (context) => HomeScreen(),
         '/admin-add-restaurant': (context) => AdminAddRestaurantPage(),
         '/restaurants-overview': (context) => RestaurantsOverviewPage(),
-
       },
     );
   }
@@ -65,6 +71,7 @@ class SplashScreen extends StatelessWidget {
       body: Center(child: CircularProgressIndicator()),
     );
   }
+
+
+
 }
-
-
