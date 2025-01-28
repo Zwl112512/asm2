776a5ff63ca82f4854c7fb477c18bb6dd4ab260c
Zwl112512 <147674826+Zwl112512@users.noreply.github.com>
2025-01-28 14:57:44 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
+import 'package:asm2/providers/chat_provider.dart';
 import 'package:asm2/screens/RestaurantsOverviewPage.dart';
+import 'package:asm2/screens/chatbot_page.dart';
 import 'package:asm2/screens/profile_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'providers/auth_provider.dart';
 import 'providers/cart_provider.dart';
-
+import 'providers/chat_provider.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'screens/home_screen.dart';
 import 'screens/login_page.dart';
 import 'screens/restaurants_page.dart';
@@ -19,12 +22,14 @@ import 'package:flutter/material.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await dotenv.load(fileName: ".env");
 
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => CustomAuthProvider()),
         ChangeNotifierProvider(create: (_) => CartProvider()),
+        ChangeNotifierProvider(create: (_) => ChatProvider()),
       ],
       child: MyApp(),
     ),
@@ -45,10 +50,14 @@ class MyApp extends StatelessWidget {
         '/home': (context) => HomeScreen(),
         '/admin-add-restaurant': (context) => AdminAddRestaurantPage(),
         '/restaurants-overview': (context) => RestaurantsOverviewPage(),
+        '/chatbot': (context) => ChatBotPage(), // 新增聊天路由
       },
     );
   }
 }
+
+
+
 
 // SplashScreen 用于判断初始路由
 class SplashScreen extends StatelessWidget {
